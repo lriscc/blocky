fcc59c7a0a472eb5eeb83bf05c42bf9ccc792660
Kendall Gifford <zettabyte@gmail.com>
2019-01-16 22:47:34 -0700

    Add experimental animation to game

--- blocky.js
+++ blocky.js
@@ -1,3 +1,5 @@
+var paddleX = 20;
+
 function main() {
 	var canvas  = document.getElementById("blocky");
 	var context = canvas.getContext("2d");
@@ -6,7 +8,13 @@ function main() {
 	// Draw the paddle:
 	var paddleWidth  = 75;
 	var paddleHeight = 15;
-	var paddleX      = 20;
+	//var paddleX      = 20;
 	var paddleY      = canvas.height - paddleHeight - 5;
 	context.fillRect(paddleX, paddleY, paddleWidth, paddleHeight);
+
+	// Move paddle to the right for next animation frame:
+	paddleX += 1;
+
+	// Request to draw next frame when browser is ready:
+	requestAnimationFrame(main);
 }
