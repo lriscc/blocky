2577da5c128bb9d78e82ba9b3103c651c07ec5eb
Kendall Gifford <zettabyte@gmail.com>
2019-01-16 22:30:59 -0700

    Move script to external file

---
+++ blocky.js
@@ -0,0 +1,6 @@
+function main() {
+	var canvas  = document.getElementById("blocky");
+	var context = canvas.getContext("2d");
+	context.fillStyle = "rgb(255, 0, 0)";
+	context.fillRect(20, 20, 300, 300);
+}
